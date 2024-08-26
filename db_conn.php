cb1e6055db70aaa6beb3b56ae3ba35c665d7701c
Pratap <103826147+Pratap7744@users.noreply.github.com>
2024-08-26 20:17:21 +0530

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -6,8 +6,8 @@ $password = "";
 
 $db_name = "test_db";
 
-$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+$conn = mysqli_connect($sname, $unmae, $password, $db_name); 
 
 if (!$conn) {
 	echo "Connection failed!";
-}
+} 
